0178b1f158abc0310a00e58667219c17e92d8634
Moinul78 <moinul@coredevs.ltd>
2023-03-25 14:21:33 +0600

    row and subtask with json added

--- src/Components/Table/ContentBody.jsx
+++ src/Components/Table/ContentBody.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import {
   IssueTypesIcon,
-  arrowDownIcon,
-  arrowRightIcon,
   circleIcon,
   dragIcon,
 } from '../../Assets/SVGcomponents';
@@ -18,130 +16,134 @@ import { subData } from '../../data';
 import IssueTypes from './IssueTypes';
 
 export default function ContentBody() {
-  const [showTask, setShowTask] = useState(false);
-  const [showSubTask, setShowSubTask] = useState(false);
-  console.log(showTask);
-  const handleTask = (id) => {
-    setShowTask(!showTask);
-    setShowSubTask(id);
-  };
   return (
     <div>
       {
         subData.map((data) => (
-          <div key={data.id} className={`hover:bg-slate-50 ${styles.tableRow}`}>
-            <div className="grid grid-cols-12 border-0 border-b-[0.063rem] border-slate-200 py-2">
-              <div className="flex justify-center items-center ">
-                {/* col-1 */}
-                <div className="flex flex-row justify-center items-center gap-x-2">
-                  <SVGIcon Icon={circleIcon} />
-                  <SVGIcon Icon={dragIcon} />
-                  <div onClick={() => handleTask(data.id)} role="contentinfo" onKeyDown={() => { }}>
-                    {
-                      showSubTask ? (<SVGIcon Icon={arrowDownIcon} />)
-                        : (<SVGIcon Icon={arrowRightIcon} />)
-                    }
+          <Row data={data} />
+        ))
+      }
+    </div>
+  );
+}
+function Row({ data }) {
+  const [showSubTask, setShowSubTask] = useState([]);
+  const [expand, setExpand] = useState(false);
+  const handleTask = (d) => {
+    setShowSubTask(d.id);
+    setExpand(!expand);
+  };
+  return (
+    <div key={data.id} className={`hover:bg-slate-50 ${styles.tableRow}`}>
+      <div className="grid grid-cols-12 border-0 border-b-[0.063rem] border-slate-200 py-2">
+        <div className="flex justify-center items-center ">
+          {/* col-1 */}
+          <div className="flex flex-row justify-center items-center gap-x-2">
+            <SVGIcon Icon={circleIcon} />
+            <SVGIcon Icon={dragIcon} />
+            <div onClick={() => handleTask(data)} role="contentinfo" onKeyDown={() => { }}>
+              {
+                expand ? (<SVGIcon Icon={data.arrowDownIcon} />)
+                  : (<SVGIcon Icon={data.ArrowRight} />)
+              }
+            </div>
+          </div>
+        </div>
+        {/* col-2 */}
+        <div className="flex flex-row justify-center items-center w-[3rem]">
+          <SVGIcon Icon={IssueTypesIcon} />
+        </div>
+        {/* col-3 */}
+        <div className="col-span-4 flex flex-row justify-start items-center gap-x-2 ml-[-3rem]">
+          <h3 className="text-heading text-sm font-medium">{data.text}</h3>
+          <div className="flex flex-row items-center">
+            <SVGIcon Icon={data.subTaskIcon} />
+            <p className="text-slate-500 font-medium text-[0.688rem]">{data.subTaskQuantity}</p>
+          </div>
+          <SVGIcon className={`${styles.summaryIcons}`} Icon={data.plusIcon} />
+          <SVGIcon className={`${styles.summaryIcons}`} Icon={data.tagIcon} />
+          <SVGIcon className={`${styles.summaryIcons}`} Icon={data.editIcon} />
+        </div>
+        {/* col-4 */}
+        <div className="grid justify-center items-center">
+          <SelectUser />
+        </div>
+        {/* col-5 */}
+        <div className="grid justify-center items-center">
+          <DropDown />
+        </div>
+        {/* col-6 */}
+        <div className="grid justify-center items-center">
+          <CalendarData />
+        </div>
+        {/* col-7 */}
+        <div className="grid justify-center items-center">
+          <Estimate />
+        </div>
+        {/* col-8 */}
+        <div className="grid justify-center items-center">
+          <SprintPoint />
+        </div>
+        {/* col-9 */}
+        <div className="grid justify-center items-center">
+          <Priority />
+        </div>
+      </div>
+      <div id="task">
+        {
+          expand
+          && subData[showSubTask]?.subTask?.map((datas) => (
+            (
+              <div key={datas.id}>
+                <div className="grid grid-cols-12 border-0 border-b-[0.063rem] border-slate-200 py-2 duration-500">
+                  {/* col-1 */}
+                  <div className="grid justify-center items-center">
+                    <div className="flex flex-row justify-center items-center gap-x-2">
+                      <SVGIcon className="invisible" Icon={datas.circleIcon} />
+                      <SVGIcon className="invisible" Icon={datas.dragIcon} />
+                      <SVGIcon Icon={datas.ArrowRight} />
+                    </div>
+                  </div>
+                  {/* col-2 */}
+                  <div className="grid justify-center items-center ml-[-4rem]">
+                    <IssueTypes />
+                  </div>
+                  {/* col-3 */}
+                  <div className="col-span-4 flex flex-row justify-start items-start gap-x-2 ">
+                    <p className="text-slate-600 font-normal text-sm text-justify">
+                      {datas.details}
+                    </p>
+                  </div>
+                  {/* col-4 */}
+                  <div className="grid justify-center items-center">
+                    <SelectUser />
+                  </div>
+                  {/* col-5 */}
+                  <div className="grid justify-center items-center">
+                    <DropDown />
+                  </div>
+                  {/* col-6 */}
+                  <div className="grid justify-center items-center">
+                    <CalendarData />
+                  </div>
+                  {/* col-7 */}
+                  <div className="grid justify-center items-center">
+                    <Estimate />
+                  </div>
+                  {/* col-8 */}
+                  <div className="grid justify-center items-center">
+                    <SprintPoint />
+                  </div>
+                  {/* col-9 */}
+                  <div className="grid justify-center items-center">
+                    <Priority />
                   </div>
                 </div>
               </div>
-              {/* col-2 */}
-              <div className="flex flex-row justify-center items-center w-[3rem]">
-                <SVGIcon Icon={IssueTypesIcon} />
-              </div>
-              {/* col-3 */}
-              <div className="col-span-4 flex flex-row justify-start items-center gap-x-2 ml-[-3rem]">
-                <h3 className="text-heading text-sm font-medium">{data.text}</h3>
-                <div className="flex flex-row items-center">
-                  <SVGIcon Icon={data.subTaskIcon} />
-                  <p className="text-slate-500 font-medium text-[0.688rem]">{data.subTaskQuantity}</p>
-                </div>
-                <SVGIcon className={`${styles.summaryIcons}`} Icon={data.plusIcon} />
-                <SVGIcon className={`${styles.summaryIcons}`} Icon={data.tagIcon} />
-                <SVGIcon className={`${styles.summaryIcons}`} Icon={data.editIcon} />
-              </div>
-              {/* col-4 */}
-              <div className="grid justify-center items-center">
-                <SelectUser />
-              </div>
-              {/* col-5 */}
-              <div className="grid justify-center items-center">
-                <DropDown />
-              </div>
-              {/* col-6 */}
-              <div className="grid justify-center items-center">
-                <CalendarData />
-              </div>
-              {/* col-7 */}
-              <div className="grid justify-center items-center">
-                <Estimate />
-              </div>
-              {/* col-8 */}
-              <div className="grid justify-center items-center">
-                <SprintPoint />
-              </div>
-              {/* col-9 */}
-              <div className="grid justify-center items-center">
-                <Priority />
-              </div>
-            </div>
-            {
-              showSubTask && (
-                subData[showSubTask].subTask.map((datas) => (
-                  (
-                    <div key={datas.id}>
-                      <div className="grid grid-cols-12 border-0 border-b-[0.063rem] border-slate-200 py-2 duration-500">
-                        {/* col-1 */}
-                        <div className="grid justify-center items-center">
-                          <div className="flex flex-row justify-center items-center gap-x-2">
-                            <SVGIcon className="invisible" Icon={circleIcon} />
-                            <SVGIcon className="invisible" Icon={dragIcon} />
-                            <SVGIcon Icon={arrowDownIcon} />
-                          </div>
-                        </div>
-                        {/* col-2 */}
-                        <div className="grid justify-center items-center ml-[-4rem]">
-                          <IssueTypes />
-                        </div>
-                        {/* col-3 */}
-                        <div className="col-span-4 flex flex-row justify-start items-start gap-x-2 ">
-                          <p className="text-slate-600 font-normal text-sm text-justify">
-                            {datas.details}
-                          </p>
-                        </div>
-                        {/* col-4 */}
-                        <div className="grid justify-center items-center">
-                          <SelectUser />
-                        </div>
-                        {/* col-5 */}
-                        <div className="grid justify-center items-center">
-                          <DropDown />
-                        </div>
-                        {/* col-6 */}
-                        <div className="grid justify-center items-center">
-                          <CalendarData />
-                        </div>
-                        {/* col-7 */}
-                        <div className="grid justify-center items-center">
-                          <Estimate />
-                        </div>
-                        {/* col-8 */}
-                        <div className="grid justify-center items-center">
-                          <SprintPoint />
-                        </div>
-                        {/* col-9 */}
-                        <div className="grid justify-center items-center">
-                          <Priority />
-                        </div>
-                      </div>
-                    </div>
-                  )
-                ))
-              )
-            }
-          </div>
-        ))
-
-      }
+            )
+          ))
+        }
+      </div>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,68 +1,106 @@
 import {
+  arrowDownIcon,
+  ArrowRight,
+  circleIcon,
+  dragIcon,
   editIcon,
   IssueTypesIcon,
   plusIcon,
   subtaskIcon,
   tagIcon,
 } from './Assets/SVGcomponents';
+// import arrowDownIcon from './Assets/Icons/arrow down.svg';
+// import ArrowRight from './Assets/Icons/Arrowright.svg';
+// import circleIcon from './Assets/Icons/circle 3.svg';
+// import dragIcon from './Assets/Icons/Drag icon.svg';
+// import editIcon from './Assets/Icons/edit-icon.svg';
+// import IssueTypesIcon from './Assets/Icons/IssueTypes.svg';
+// import plusIcon from './Assets/Icons/plus-sign.svg';
+// import subtaskIcon from './Assets/Icons/Sub task.svg';
+// import tagIcon from './Assets/Icons/tag-icon.svg';
 
 const subData = [
+  {
+    id: 0,
+    issueTypesIcon: IssueTypesIcon,
+    text: 'Custom Integration',
+    subTaskIcon: subtaskIcon,
+    subTaskQuantity: 0,
+    arrowDownIcon,
+    ArrowRight,
+    plusIcon,
+    tagIcon,
+    editIcon,
+    dragIcon,
+    subTask: [],
+    isOpen: false,
+  },
   {
     id: 1,
     issueTypesIcon: IssueTypesIcon,
     text: 'Custom Integration',
     subTaskIcon: subtaskIcon,
-    subTaskQuantity: 0,
+    subTaskQuantity: 2,
+    arrowDownIcon,
+    ArrowRight,
     plusIcon,
     tagIcon,
     editIcon,
+    dragIcon,
     subTask: [
-
+      {
+        id: 1,
+        circleIcon,
+        dragIcon,
+        ArrowRight,
+        details: 'As a new Certified ScrumMaster, once my name has been loaded to the registry I am sent an email welcoming me to the Scrum Alliance and containing instructions on how to register/activate my membership',
+      },
+      {
+        id: 2,
+        circleIcon,
+        dragIcon,
+        ArrowRight,
+        details: 'As a new Certified ScrumMaster, once my name has been loaded to the registry I am sent an email welcoming me to the Scrum Alliance and containing instructions on how to register/activate my membership',
+      },
     ],
+    isOpen: false,
   },
   {
     id: 2,
     issueTypesIcon: IssueTypesIcon,
     text: 'Custom Integration',
     subTaskIcon: subtaskIcon,
-    subTaskQuantity: 2,
-    plusIcon,
-    tagIcon,
-    editIcon,
-    subTask: [
-      {
-        id: 1,
-        details: 'As a new Certified ScrumMaster, once my name has been loaded to the registry I am sent an email welcoming me to the Scrum Alliance and containing instructions on how to register/activate my membership',
-      },
-      {
-        id: 2,
-        details: 'As a new Certified ScrumMaster, once my name has been loaded to the registry I am sent an email welcoming me to the Scrum Alliance and containing instructions on how to register/activate my membership',
-      },
-    ],
-  },
-  {
-    id: 3,
-    issueTypesIcon: IssueTypesIcon,
-    text: 'Custom Integration',
-    subTaskIcon: subtaskIcon,
     subTaskQuantity: 3,
+    arrowDownIcon,
+    ArrowRight,
     plusIcon,
     tagIcon,
     editIcon,
+    dragIcon,
     subTask: [
       {
         id: 1,
+        circleIcon,
+        dragIcon,
+        ArrowRight,
         details: 'As a new Certified ScrumMaster, once my name has been loaded to the registry I am sent an email welcoming me to the Scrum Alliance and containing instructions on how to register/activate my membership',
       },
       {
         id: 2,
+        circleIcon,
+        dragIcon,
+        ArrowRight,
         details: 'As a new Certified ScrumMaster, once my name has been loaded to the registry I am sent an email welcoming me to the Scrum Alliance and containing instructions on how to register/activate my membership',
       },
       {
         id: 3,
+        circleIcon,
+        dragIcon,
+        ArrowRight,
         details: 'As a new Certified ScrumMaster, once my name has been loaded to the registry I am sent an email welcoming me to the Scrum Alliance and containing instructions on how to register/activate my membership',
       },
     ],
+    isOpen: false,
   },
 ];
 const personDetails = {
